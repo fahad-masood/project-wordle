97134d035deb616447146159aa4eeeb626275eca
Fahad <fahadmasood03@gmail.com>
2023-10-06 01:59:26 +0530

    Complete Exercise-2 Keeping Track of Guesses

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,28 +1,31 @@
 import React from "react";
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState("");
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ guess });
-    setGuess("");
+    // console.log({ tentativeGuess });
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess("");
   };
   return (
-    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-      <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        required
-        id="guess-input"
-        type="text"
-        value={guess}
-        minLength={5} //not working as of now because of toUpperCase(), so pattern is being used
-        maxLength={5}
-        pattern="[a-zA-Z]{5}"
-        title="5 letter word (Use alphabets only)"
-        onChange={(e) => setGuess(e.target.value.toUpperCase())}
-      />
-    </form>
+    <>
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          required
+          id="guess-input"
+          type="text"
+          value={tentativeGuess}
+          minLength={5} //not working as of now because of toUpperCase(), so pattern is being used
+          maxLength={5}
+          pattern="[a-zA-Z]{5}"
+          title="5 letter word (Use alphabets only)"
+          onChange={(e) => setTentativeGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+    </>
   );
 }
 
