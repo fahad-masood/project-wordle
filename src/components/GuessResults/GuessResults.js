ee177ccad4cd882794cd29757436966b5a6e590f
Fahad <fahadmasood03@gmail.com>
2023-10-19 04:56:51 +0530

    Complete Exercise-4 Game logic

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,13 +3,12 @@ import Guess from "../Guess/Guess";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 
-function GuessResults({ guesses }) {
-  console.log(range(0, 6));
+function GuessResults({ guesses, answer }) {
   return (
     <div>
       <div className="guess-results">
         {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-          <Guess key={num} value={guesses[num]} />
+          <Guess key={num} value={guesses[num]} answer={answer} />
         ))}
       </div>
     </div>
